80750ba3c5380fd1e9d9595c80bae57206aef776
gabreuvcr <gabreuvcr@gmail.com>
2020-10-15 19:56:02 -0300

    Commit 1

---
+++ Main.java
@@ -0,0 +1,16 @@
+class Main {
+  public static void main(String[] args) {
+    Movie naruto = new Movie("Naruto", 1);
+    Movie sherlock = new Movie("Sherlock Holmes", 0);
+
+    Rental rentalNaruto = new Rental(naruto, 3);
+    Rental rentalSherlock = new Rental(sherlock, 3);
+
+    Customer jao = new Customer("Jao");
+
+    jao.addRental(rentalNaruto);
+    jao.addRental(rentalSherlock);
+
+    System.out.println(jao.statement());
+  }
+}
